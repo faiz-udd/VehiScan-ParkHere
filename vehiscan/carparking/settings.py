b8388b37297b1de95d69054f8859f6442af286c0
=Faiz uddin <irfanilm524@gmail.com>
2025-01-23 17:03:53 +0500

    Carparking app

--- vehiscan/carparking/settings.py
+++ vehiscan/carparking/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'carParkingFrontend.apps.CarparkingfrontendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
