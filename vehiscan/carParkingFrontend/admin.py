7baea82c4d5b81894eace6cfe84111a0c71d1416
=Faiz uddin <irfanilm524@gmail.com>
2025-01-31 11:02:01 +0500

    Project Updates

--- vehiscan/carParkingFrontend/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from carParkingFrontend.models import ParkingLot, ParkingLotMonitor
-
-# Register your models here.
-
-admin.site.register(ParkingLot)
-admin.site.register(ParkingLotMonitor)
